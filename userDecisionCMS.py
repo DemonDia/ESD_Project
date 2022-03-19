bd0e244208a0c1c3f4014793230015ccd140d45f
xDemonDia <siang-meng@live.com>
2022-03-19 14:56:05 +0800

    Tidied up the microservices (didnt include AMQP)

--- userDecisionCMS.py
+++ userDecisionCMS.py
@@ -9,16 +9,6 @@ CORS(app)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 ApplicationSMS = "http://127.0.0.1:5003/applications"
 
-firebaseConfig = {
-  "apiKey": "AIzaSyCCD_YMl1GVhCacEWtj424cMrmqHWqyzw0",
-  "authDomain": "jobmicroservice-13a0c.firebaseapp.com",
-  "databaseURL": "https://jobmicroservice-13a0c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "jobmicroservice-13a0c",
-  "storageBucket": "jobmicroservice-13a0c.appspot.com",
-  "messagingSenderId": "209394015065",
-  "appId": "1:209394015065:web:b6612fd69caeaea6124a48"
-}
-
 @app.route("/get_applications")
 def getUsers(UID):
     try:
@@ -31,7 +21,7 @@ def owner_get_applications(UID):
         applications = invoke_http(ApplicationSMS+"/user/"+UID,method = "GET")
         return applications
     except Exception as e:
-        return None
+        return "NOT OK"
 
 
 
